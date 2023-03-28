0f65b8e3735c40bde6b6a1abf9c3a4b403cf3da3
Черепахин Василий <vasi.che@gmail.com>
2023-03-28 15:04:55 +0500

    Создание проекта

---
+++ Readme.md
@@ -0,0 +1,5 @@
+### DTO для проекта Animals 
+
+Сам проект СЕРВИСА находится [https://github.com/cherepakhin/animals](https://github.com/cherepakhin/animals)
+
+Проект выделен из основного проекта для использования в смежных проектах и behave тестирования внешними инструментами 
---
+++ src/main/java/ru/perm/v/animals/dto/AnimalDto.java
@@ -0,0 +1,28 @@
+package ru.perm.v.animals.dto;
+
+public class AnimalDto {
+
+    private Long id = 0L;
+    private String name = "";
+
+    public AnimalDto(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
